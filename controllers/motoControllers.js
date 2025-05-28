dc54783b9c4c8f8ef9f8cd3a93dbed5ce82043f8
FernandoSalicandro <studio.salicandro@gmail.com>
2025-05-28 15:24:26 +0200

    feat: implementato filtro multiplo nell'index

--- controllers/motoControllers.js
+++ controllers/motoControllers.js
@@ -4,22 +4,52 @@ import motoArr from "../motoBlog.js";
 //IMPOSTO ED ESPORTO LE FUNZIONI PER LE OPERAZIONI CRUD NELLE ROUTE
 
 const index = (req, res) => {
+   //definisco l'oggetto della query
+    const query = req.query;
 
-  const motoFilter = req.query.marca;
-
-  if (motoFilter) {
-    const filtered = motoArr.filter(
-      moto => moto.marca.toLowerCase() === motoFilter.toLowerCase()
-    );
-
-    if (filtered.length === 0) {
-      return res.status(404).json({ error: `Nessuna moto trovata per la marca '${motoFilter}'` });
+    //setto che se la query è vuota ritorno tutto l'array (essendo index)
+    const queryArr = [];
+    for(let key in query) {
+        queryArr.push(key)
     }
+   
+    if(queryArr.length===0) return res.json({motoArr})
 
-    return res.json(filtered);
-  } else {
-    return res.json(motoArr);
-  }
+    //nel caso in cui la query non è vuota filtro dinamicamente
+
+    // Avvio il filtraggio per ogni oggetto "moto" nell'array motoArr
+    const filtered = motoArr.filter(moto => {
+
+        // tramite every sull'array delle chiavi contenute nella query
+        //  controllo che la moto soddisfi tutte le condizioni specificate nella query
+        
+        return queryArr.every(chiave => {
+
+            // Prendo il valore del filtro passato nei parametri e lo rendo case insensitive
+            const queryFilter = query[chiave].toLowerCase();
+
+            // Prendo il valore corrispondente nella moto
+            const motoFilter = moto[chiave];
+
+
+            // Se è una stringa, confronto con la query string  usando includes
+            if (typeof motoFilter === "string") {
+                return motoFilter.toLowerCase().includes(queryFilter)
+            }
+
+            //se non è una stringa lo trasformo in stringa e poi lo confronto
+            return motoFilter.toString() === queryFilter;
+
+
+        })
+
+
+    })
+    //se il risultato del filtraggio è un array vuoto restituisco errore e messaggio di moto non trovata.
+    if (filtered.length === 0) return res.status(404).json({ Error: "Moto non trovata" })
+
+    //altrimenti restituisco il risultato.
+    res.json({ filtered })
 };
 
 
